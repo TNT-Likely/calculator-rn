b7d3ef060d72455c17b9abc36cf0951960e1a43c
likely <sunxiaoyes@outlook.com>
2024-09-12 11:08:35 +0800

    feat: 引入antd

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {Button, Provider, Toast} from '@ant-design/react-native';
 
 import {
   Colors,
@@ -71,25 +72,16 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text>222</Text>
+          <Provider>
+            <Button onPress={() => Toast.info('This is a toast tips')}>
+              Start
+            </Button>
+          </Provider>
         </View>
       </ScrollView>
     </SafeAreaView>
